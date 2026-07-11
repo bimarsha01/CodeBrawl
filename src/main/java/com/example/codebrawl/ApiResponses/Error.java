e6b084015a54b49f486dccd2d058d9a0b5c2abf3
bimarsha <bimarshaghimire09@gmail.com>
2026-07-11 23:31:25 +0545

    feat/globalExceptionHandler

---
+++ src/main/java/com/example/codebrawl/ApiResponses/Error.java
@@ -0,0 +1,17 @@
+package com.example.codebrawl.ApiResponses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiErrors<T> {
+    private String error;
+    private Boolean status;
+    private T data;
+
+}
---
+++ src/main/java/com/example/codebrawl/ExceptionHandling/InvalidCredentialsException.java
@@ -0,0 +1,4 @@
+package com.example.codebrawl.ExceptionHandling;
+
+public class InvalidCredentialsException {
+}
