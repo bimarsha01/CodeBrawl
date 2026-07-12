b9d6255e3a95b30874946fd00bedeb55baf99a99
bimarsha <bimarshaghimire09@gmail.com>
2026-07-12 21:53:29 +0545

    feat/used field constants

--- src/main/java/com/example/codebrawl/Dtos/AuthDto/LoginRequestDto.java
+++ src/main/java/com/example/codebrawl/Dtos/AuthDto/LoginRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.codebrawl.Dtos.AuthDto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +9,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequestDto {
+    @NotBlank(message = "Username is required")
     private String username;
+    @NotBlank(message = "Password is required")
     private String password;
 }
--- src/main/java/com/example/codebrawl/Dtos/AuthDto/SignUpRequestDto.java
+++ src/main/java/com/example/codebrawl/Dtos/AuthDto/SignUpRequestDto.java
@@ -1,5 +1,9 @@
 package com.example.codebrawl.Dtos.AuthDto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +12,27 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignUpRequestDto {
+    @NotBlank(message = "Username is Required")
+    @Size(min =3 , max = 20 , message = "Username must be between 3 and 20 characters")
+    @Pattern(
+            regexp = "^[a-zA-Z0-9_]+$",
+            message = "Username can only contain letters, numbers, and underscores"
+    )
     private String username;
+    @NotBlank(message = "Password is Required")
+    @Size(min =6 , max = 100 , message = "Password must be between 6 and 100 characters")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character"
+    )
     private String password;
-    private String contacts;
+    @NotBlank(message = "Contact is required")
+    @Pattern(
+            regexp = "^(98|97)\\d{8}$",
+            message = "Contact must be a valid phone number starting with 98 or 97"
+    )
+    private String contactNumber;
+    @NotBlank(message = "Email is Required")
+    @Email(message = "Invalid email format")
     private String email;
 }
