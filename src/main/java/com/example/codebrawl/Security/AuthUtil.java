ee44e6518dad974f519854c905613d27effb7987
bimarsha <bimarshaghimire09@gmail.com>
2026-07-23 22:19:51 +0545

    feat/added access and refresh token

--- src/main/java/com/example/codebrawl/Security/AuthUtil.java
+++ src/main/java/com/example/codebrawl/Security/AuthUtil.java
@@ -15,16 +15,33 @@ public class AuthUtil {
     @Value("${jwt.secretKey}")
     private String secretKey;
 
-    public SecretKey getSecretKey(){
+
+    public SecretKey getAccessTokenSecretKey() {
         return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
     }
-    public String getAccessToken(UserEntity user){
+
+    public SecretKey getRefreshTokenSecretKey() {
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String getAccessToken(UserEntity user) {
         return Jwts.builder()
                 .subject(user.getUsername())
-                .claim("userId" , user.getId().toString())
-                .signWith(getSecretKey())
+                .claim("userId", user.getId().toString())
+                .claim("userRole", user.getRole())
+                .signWith(getAccessTokenSecretKey())
                 .issuedAt(new Date())
                 .expiration(new Date(System.currentTimeMillis() + 86400000))
+                .compact();
+    }
+
+    public String getRefreshToken(UserEntity user) {
+        return Jwts.builder()
+                .subject(user.getUsername())
+                .claim("userId", user.getId())
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 7L * 24 * 60 * 60 * 1000))
+                .signWith(getRefreshTokenSecretKey())
                 .compact();
     }
 }
